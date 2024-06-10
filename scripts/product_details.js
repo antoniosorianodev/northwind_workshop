32b5693d9da0cc24f2c93ba8bb0f1f25a28ac834
Antonio <asoriano@appdev.yearup.org>
2024-06-10 12:54:58 -0500

    added display card for product details page

--- scripts/product_details.js
+++ scripts/product_details.js
@@ -1,5 +1,31 @@
 "use strict"
 
 window.onload = () => {
+    const urlParams = new URLSearchParams(location.search);
 
+    // we can access the individual params by calling .get on the variable that holds the
+    // and requesting by name
+
+    if (urlParams.has("productId")) {
+        // if we have a product id, display it's details
+        displayProductDetails(urlParams.get("productId"));
+
+    } else {
+        // let them know we didn't have a valid product id and send them back to home page
+        alert("no valid product id");
+        window.location.href = "./index.html";
+    }
+}
+
+async function displayProductDetails(productId) {
+    let response = await fetch(`http://localhost:8081/api/products/${productId}`, {});
+    let data = await response.json();
+
+    for (const property in data) {
+        let dataToShow = data[`${property}`]
+        if (property === "unitPrice") {
+            dataToShow = `$${Number(dataToShow).toFixed(2)}`;
+        }
+        document.querySelector(`#${property}Section`).innerHTML = `<b>${property}:</b> ` + dataToShow;
+    }
 }
